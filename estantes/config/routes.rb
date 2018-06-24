479ba21387c17e449c22ccef7cbf5f0498e0f4b6
vzaffalon <zaffalonvictor@gmail.com>
2018-06-24 18:43:42 -0300

    criando tabelas de estantes, exemplares

--- estantes/config/routes.rb
+++ estantes/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  resources :users
   resources :resenhas
   resources :estantes
   resources :exemplars
   resources :livros
-  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- estantes/db/migrate/20180624214223_create_users.rb
+++ estantes/db/migrate/20180624214223_create_users.rb
@@ -1,10 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string :nome
-      t.string :apelido
-      t.string :telefone
-      t.string :senha
 
       t.timestamps
     end
